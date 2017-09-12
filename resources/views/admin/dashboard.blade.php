46517afdbd94b43f6101ea2bfdc04f2c6541a3e5
Leo Fontolan <leo@bluefive.com.br>
2017-09-12 14:58:39 -0300

    Traduzindo Mensagens da Aplicacao e Reset de Senha

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container">
+    <h1>Área Administrativa</h1>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,18 +15,45 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+
+//Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
 
-Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'can:admin'], function(){
-    Route::get('/', function () {
-        return "Area Administrativa";
+Route::group([
+
+    'prefix' => 'admin',
+    'as' => 'admin.',
+    'namespace' => 'Admin\\'
+
+
+], function () {
+
+
+    Route::name('login')->get('login', 'Auth\LoginController@showLoginForm');
+
+    Route::post('login', 'Auth\LoginController@login');
+
+    Route::group(['middleware' => 'can:admin'], function () {
+        Route::name('logout')->post('logout', 'Auth\LoginController@logout');
+
+        Route::get('dashboard', function(){
+            return view('admin.dashboard');
+        });
     });
+
+
 });
 
-Route::get('/force-login', function(){
+Route::get('/force-login', function () {
 
     \Auth::loginUsingId(1);
 });
